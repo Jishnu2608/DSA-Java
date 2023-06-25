fbef9a9881962642d7fc098ecc85e037268c34e1
Jishnu2608 <jishnudeepborah@gmail.com>
2023-06-26 01:28:18 +0530

    LinkedList

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -2,12 +2,19 @@ package LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        Linkedlist list = new Linkedlist();
+        Singly_Linkedlist list = new Singly_Linkedlist();
         list.insertFirst(3);
         list.insertFirst(4);
         list.insertFirst(5);
         list.insertFirst(1);
-
+        list.insertLast(99);
+        list.insert(100, 3);
+        list.display();
+        System.out.println(list.deleteFirst());
+        list.display();
+        System.out.println(list.deleteLast());
+        list.display();
+        System.out.println(list.delete(2));
         list.display();
     }
 }
