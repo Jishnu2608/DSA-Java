8107d52000b25a26ce02410e1abd8d8d79d8a338
Jishnu2608 <jishnudeepborah@gmail.com>
2023-06-26 00:27:31 +0530

    LinkedList

---
+++ src/LinkedList/Doubly_Linkedlist.java
@@ -0,0 +1,42 @@
+package LinkedList;
+
+public class Doubly_Linkedlist {
+    Node head;
+
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next=head;
+        node.prev=null;
+        if(head!=null){
+            head.prev=node;
+        }
+        head=node;
+    }
+
+
+    public void display(){
+        Node node = head;
+
+        while(node!=null){
+            System.out.println(node.val+" -> ");
+            node=node.next;
+        }
+        System.out.println();
+    }
+
+
+    private class Node{
+        int val;
+        Node next, prev;
+
+        public Node(int val){
+            this.val=val;
+        }
+
+        public Node(int val, Node next, Node prev){
+            this.val=val;
+            this.next=next;
+            this.prev=prev;
+        }
+    }
+}
---
+++ src/LinkedList/Main.java
@@ -0,0 +1,13 @@
+package LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        Linkedlist list = new Linkedlist();
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertFirst(5);
+        list.insertFirst(1);
+
+        list.display();
+    }
+}
